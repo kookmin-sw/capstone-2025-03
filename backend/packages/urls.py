df3bce4e5c35d2e3b145ded30c1b2b98a7cdded8
gwondoo <6568kdh@naver.com>
2025-02-21 20:24:51 +0900

    Update backend

--- backend/packages/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import package_list
-
-urlpatterns = [
-    path('', package_list, name='package_list'),
-]
--- backend/products/forms.py
+++
@@ -1,8 +0,0 @@
-# products/forms.py
-from django import forms
-from .models import Product
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['name', 'image']
