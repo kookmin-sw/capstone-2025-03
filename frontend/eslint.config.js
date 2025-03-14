d2c5b333c06fd59146c7e8440d56a1cc07c668e8
ureChanger <rlfgus8708@gmail.com>
2025-03-14 13:57:20 +0900

    [fix] no-unused-vars

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,8 +23,9 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      "@typescript-eslint/no-unused-vars": "off",
-       "@typescript-eslint/no-explicit-any": "warn",
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
 )
