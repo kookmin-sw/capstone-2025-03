ca68d54198fe468b31122c4c387621e58e0052f0
kimyounghee425 <doctorsean0425@gmail.com>
2025-03-27 16:43:30 +0900

    [feat] sellerProduct atom, selector 생성

---
+++ frontend/src/hooks/useSellerProduct.ts
@@ -0,0 +1,8 @@
+// sellerProductState 에 대한 커스텀 훅
+import { useRecoilState } from "recoil";
+import { sellerProductState } from "../recoil/sellerProductState";
+import { createProductInService } from "../services/productService";
+
+export const useSellerProduct = () => {
+    
+}
