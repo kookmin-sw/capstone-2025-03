27a16dcf8321a423a1a008c47b70503155a31893
kimyounghee425 <doctorsean0425@gmail.com>
2025-05-05 18:40:41 +0900

    [feat] 소개 페이지 폴더 생성, FullPageCarousel 구현

---
+++ frontend/src/pages/landing-page-introduce/components/IntroduceMain.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const ProgressBarContainer = styled.div`
+    background: '#BCBCBC';
+`;
+
+export const ProgressBar = styled.div`
+    background: '#777777';
+    height: '0.4rem';
+    margin-bottom: '1.6rem';
+    transition: 'all ease-out 0.3s';
+`;
+
+export const CarouselContainer = styled.div`
+    overflow: 'hidden';
+`;
+
+export const CarouselWrapper = styled.div`
+    display: 'flex';
+    gap: '2rem';
+`;
+
+export const CarouselSlider = styled.div`
+    flex: '0 0 100%';
+    min-width: 0;
+    user-select: 'none';
+`;
